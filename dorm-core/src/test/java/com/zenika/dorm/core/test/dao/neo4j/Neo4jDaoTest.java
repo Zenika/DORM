141e900037708e0ecbfdc5aff32714a85f446a0d
Antoine Rouaze <antoine.rouaze@zenika.com>
2011-07-19 18:06:15 +0200

    Add Dao Neo4j push methode using Neo4j batch

---
+++ dorm-core/src/test/java/com/zenika/dorm/core/test/dao/neo4j/Neo4jDaoTest.java
@@ -0,0 +1,7 @@
+package com.zenika.dorm.core.test.dao.neo4j;
+
+/**
+ * @author Antoine ROUAZE <antoine.rouaze AT zenika.com>
+ */
+public class Neo4jDaoTest {
+}
---
+++ dorm-core/src/test/java/com/zenika/dorm/core/test/dao/neo4j/util/Neo4jParserTest.java
@@ -0,0 +1,7 @@
+package com.zenika.dorm.core.test.dao.neo4j.util;
+
+/**
+ * @author Antoine ROUAZE <antoine.rouaze AT zenika.com>
+ */
+public class Neo4jParserTest {
+}
