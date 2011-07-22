9b9893d170ce56a1b3f992973343a755b760b5cb
Lukasz Piliszczuk <lukasz.piliszczuk@zenika.com>
2011-07-22 12:15:22 +0200

    Externalize generic fixtures from the dorm fixtures in an abstract fixture class.

---
+++ dorm-core/src/test/java/com/zenika/dorm/core/test/helper/AbstractTestFixtures.java
@@ -0,0 +1,123 @@
+package com.zenika.dorm.core.test.helper;
+
+import com.zenika.dorm.core.graph.Dependency;
+import com.zenika.dorm.core.graph.DependencyNode;
+import com.zenika.dorm.core.graph.impl.DefaultDependency;
+import com.zenika.dorm.core.graph.impl.DefaultDependencyNode;
+import com.zenika.dorm.core.model.DormFile;
+import com.zenika.dorm.core.model.DormMetadata;
+import com.zenika.dorm.core.model.DormMetadataExtension;
+import com.zenika.dorm.core.model.DormRequest;
+import com.zenika.dorm.core.model.impl.DefaultDormFile;
+import com.zenika.dorm.core.model.impl.DefaultDormMetadata;
+import com.zenika.dorm.core.model.impl.DefaultDormMetadataExtension;
+import com.zenika.dorm.core.model.impl.DefaultDormRequest;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Lukasz Piliszczuk <lukasz.piliszczuk AT zenika.com>
+ */
+public abstract class AbstractTestFixtures {
+
+    /**
+     * Dorm file
+     */
+    private String filenameWithExtension = "testfile.jar";
+    private String filename = "testfile";
+    private String filenameExtension = "jar";
+    private File file = new File("/tmp/testfile.jar");
+
+    /**
+     * Metadata
+     */
+    private String version = "1.0";
+
+    /**
+     * Return the metadata extension
+     *
+     * @return
+     */
+    public abstract DormMetadataExtension getMetadataExtension();
+
+    /**
+     * Return the request properties that are specific to the extension
+     *
+     * @return
+     */
+    public abstract Map<String, String> getRequestPropertiesForExtension();
+
+    public Map<String, String> getRequestPropertiesWithoutFile() {
+        Map<String, String> properties = new HashMap<String, String>();
+        properties.put(DormRequest.ORIGIN, DefaultDormMetadataExtension.ORIGIN);
+        properties.put(DormRequest.VERSION, version);
+
+        Map<String, String> extensionProperties = getRequestPropertiesForExtension();
+
+        if (null != extensionProperties) {
+            properties.putAll(extensionProperties);
+        }
+
+        return properties;
+    }
+
+    public Map<String, String> getRequestPropertiesWithFile() {
+        Map<String, String> properties = getRequestPropertiesWithoutFile();
+        properties.put(DormRequest.FILENAME, filename);
+        return properties;
+    }
+
+    public DormRequest getRequestWithoutFile() {
+        return DefaultDormRequest.create(getRequestPropertiesWithoutFile());
+    }
+
+    public DormRequest getRequestWithFile() {
+        return DefaultDormRequest.create(getRequestPropertiesWithFile(), file);
+    }
+
+    public DormMetadata getMetadata() {
+        return DefaultDormMetadata.create(version, getMetadataExtension());
+    }
+
+    public DormFile getDormFile() {
+        return DefaultDormFile.create(filename, filenameExtension, file);
+    }
+
+    public Dependency getDependencyWithoutFile() {
+        return DefaultDependency.create(getMetadata());
+    }
+
+    public Dependency getDependencyWithFile() {
+        return DefaultDependency.create(getMetadata(), getDormFile());
+    }
+
+    public DependencyNode getNodeWithoutFile() {
+        return DefaultDependencyNode.create(getDependencyWithoutFile());
+    }
+
+    public DependencyNode getNodeWithFile() {
+        return DefaultDependencyNode.create(getDependencyWithFile());
+    }
+
+    public String getFilenameWithExtension() {
+        return filenameWithExtension;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public String getFilenameExtension() {
+        return filenameExtension;
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+}
--- dorm-core/src/test/java/com/zenika/dorm/core/test/helper/DormTestFixtures.java
+++ dorm-core/src/test/java/com/zenika/dorm/core/test/helper/DormTestFixtures.java
@@ -1,19 +1,9 @@
 package com.zenika.dorm.core.test.helper;
 
-import com.zenika.dorm.core.graph.Dependency;
-import com.zenika.dorm.core.graph.DependencyNode;
-import com.zenika.dorm.core.graph.impl.DefaultDependency;
-import com.zenika.dorm.core.graph.impl.DefaultDependencyNode;
-import com.zenika.dorm.core.model.DormFile;
-import com.zenika.dorm.core.model.DormMetadata;
 import com.zenika.dorm.core.model.DormMetadataExtension;
-import com.zenika.dorm.core.model.DormRequest;
-import com.zenika.dorm.core.model.impl.DefaultDormFile;
-import com.zenika.dorm.core.model.impl.DefaultDormMetadata;
 import com.zenika.dorm.core.model.impl.DefaultDormMetadataExtension;
-import com.zenika.dorm.core.model.impl.DefaultDormRequest;
+import com.zenika.dorm.core.processor.impl.DormProcessor;
 
-import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,7 +12,7 @@ import java.util.Map;
  *
  * @author Lukasz Piliszczuk <lukasz.piliszczuk AT zenika.com>
  */
-public class DormTestFixtures {
+public class DormTestFixtures extends AbstractTestFixtures {
 
     /**
      * Dorm metadata extension
@@ -31,67 +21,16 @@ public class DormTestFixtures {
     private String qualifier = "testname";
     private String origin = "dorm";
 
-    /**
-     * Dorm file
-     */
-    private String filenameWithExtension = "testfile.jar";
-    private String filename = "testfile";
-    private String filenameExtension = "jar";
-    private File file = new File("/tmp/testfile.jar");
-
-    /**
-     * Metadata
-     */
-    private String version = "1.0";
-
-    public Map<String, String> getRequestPropertiesWithoutFile() {
-        Map<String, String> properties = new HashMap<String, String>();
-        properties.put(DormRequest.ORIGIN, DefaultDormMetadataExtension.ORIGIN);
-        properties.put(DormRequest.VERSION, version);
-        properties.put("name", name);
-        return properties;
-    }
-
-    public Map<String, String> getRequestPropertiesWithFile() {
-        Map<String, String> properties = getRequestPropertiesWithoutFile();
-        properties.put(DormRequest.FILENAME, filename);
-        return properties;
-    }
-
-    public DormRequest getRequestWithoutFile() {
-        return DefaultDormRequest.create(getRequestPropertiesWithoutFile());
-    }
-
-    public DormRequest getRequestWithFile() {
-        return DefaultDormRequest.create(getRequestPropertiesWithFile(), file);
-    }
-
+    @Override
     public DormMetadataExtension getMetadataExtension() {
         return new DefaultDormMetadataExtension(name);
     }
 
-    public DormMetadata getMetadata() {
-        return DefaultDormMetadata.create(version, getMetadataExtension());
-    }
-
-    public DormFile getDormFile() {
-        return DefaultDormFile.create(filename, filenameExtension, file);
-    }
-
-    public Dependency getDependencyWithoutFile() {
-        return DefaultDependency.create(getMetadata());
-    }
-
-    public Dependency getDependencyWithFile() {
-        return DefaultDependency.create(getMetadata(), getDormFile());
-    }
-
-    public DependencyNode getNodeWithoutFile() {
-        return DefaultDependencyNode.create(getDependencyWithoutFile());
-    }
-
-    public DependencyNode getNodeWithFile() {
-        return DefaultDependencyNode.create(getDependencyWithFile());
+    @Override
+    public Map<String, String> getRequestPropertiesForExtension() {
+        Map<String, String> properties = new HashMap<String, String>();
+        properties.put(DormProcessor.METADATA_NAME, name);
+        return properties;
     }
 
     public String getName() {
@@ -100,26 +39,6 @@ public class DormTestFixtures {
 
     public String getQualifier() {
         return qualifier;
-    }
-
-    public String getFilenameWithExtension() {
-        return filenameWithExtension;
-    }
-
-    public String getFilename() {
-        return filename;
-    }
-
-    public String getFilenameExtension() {
-        return filenameExtension;
-    }
-
-    public File getFile() {
-        return file;
-    }
-
-    public String getVersion() {
-        return version;
     }
 
     public String getOrigin() {
