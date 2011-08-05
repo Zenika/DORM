8bfb0aa19dbbfcdcb06a47f2fadc9b5d8a94e80f
Lukasz Piliszczuk <lukasz.piliszczuk@zenika.com>
2011-08-05 18:38:48 +0200

    Add partial maven web service.

--- dorm-maven/src/main/java/com/zenika/dorm/maven/ws/resource/MavenResource.java
+++ dorm-maven/src/main/java/com/zenika/dorm/maven/ws/resource/MavenResource.java
@@ -1,24 +1,38 @@
-//package com.zenika.dorm.maven.ws.resource;
-//
-//import com.google.inject.Inject;
-//import com.sun.jersey.multipart.FormDataParam;
-//import com.zenika.dorm.core.helper.DormFileHelper;
-//import com.zenika.dorm.core.model.old.DormArtifact;
-//import com.zenika.dorm.maven.model.impl.DormMavenMetadata;
-//import com.zenika.dorm.maven.service.MavenService;
-//
-//import javax.ws.rs.*;
-//import javax.ws.rs.core.MediaType;
-//import javax.ws.rs.core.Response;
-//import java.io.File;
-//
-//@Path("maven")
-//@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-//public class MavenResource {
-//
-//    @Inject
-//    private MavenService service;
-//
+package com.zenika.dorm.maven.ws.resource;
+
+import com.google.inject.Inject;
+import com.zenika.dorm.core.processor.Processor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+
+@Path("maven")
+@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+public class MavenResource {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MavenResource.class);
+
+    @Inject
+    private Processor processor;
+
+    public MavenResource() {
+        LOG.info("Init maven resource as web service");
+    }
+
+    @POST
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response push(MultivaluedMap<String, String> params) {
+        LOG.info("Push maven with values : " + params);
+        return Response.status(Response.Status.OK).build();
+    }
+
 //    @POST
 //    @Consumes(MediaType.MULTIPART_FORM_DATA)
 //    @Path("{groupid}/{artifactid}/{version}/{filename}")
@@ -62,5 +76,5 @@
 //
 //        return Response.status(Response.Status.OK).build();
 //    }
-//
-//}
+
+}
