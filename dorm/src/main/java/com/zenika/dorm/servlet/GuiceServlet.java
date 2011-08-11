2c78b915a76ac5a2d286e908e31bf4bd53a4f9aa
Guillaume Darmont <guillaume.darmont@zenika.com>
2011-08-11 13:52:04 +0200

    Added JUL to SLF4J configuration Excluded log4j from classpath

--- dorm/src/main/java/com/zenika/dorm/servlet/GuiceServlet.java
+++ dorm/src/main/java/com/zenika/dorm/servlet/GuiceServlet.java
@@ -4,6 +4,9 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.servlet.GuiceServletContextListener;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+
 /**
  * @author Lukasz Piliszczuk <lukasz.piliszczuk AT zenika.com>
  */
@@ -12,6 +15,12 @@ public class GuiceServlet extends GuiceServletContextListener {
     @Override
     protected Injector getInjector() {
         return Guice.createInjector(new GuiceModule());
+    }
+
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        ServletContext servletContext = servletContextEvent.getServletContext();
+        JULConfigurer.configureJULtoSLF4J(servletContext);
+        super.contextInitialized(servletContextEvent);
     }
 
 }
