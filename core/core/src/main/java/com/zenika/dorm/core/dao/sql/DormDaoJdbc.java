785025f6b3f324e38aae7d5099fe173fad3f0291
Antoine Rouaze <antoine.rouaze@zenika.com>
2011-09-14 17:06:59 +0200

    Refactor Jdbc Dao.

--- core/core/src/main/java/com/zenika/dorm/core/dao/sql/DormDaoJdbc.java
+++ core/core/src/main/java/com/zenika/dorm/core/dao/sql/DormDaoJdbc.java
@@ -2,6 +2,8 @@ package com.zenika.dorm.core.dao.sql;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
 import com.zenika.dorm.core.dao.DormDao;
 import com.zenika.dorm.core.guice.module.DormCoreModule;
 import com.zenika.dorm.core.model.DormMetadata;
@@ -12,10 +14,12 @@ import java.util.Map;
 
 public class DormDaoJdbc implements DormDao {
 
+    @Inject
+    private Injector injector;
+
     @Override
     public void saveMetadata(final DormMetadata metadata) {
-        JDBCSinglePushService jdbcSinglePushService = Guice.createInjector(
-                new DormCoreModule(),
+        JDBCSinglePushService jdbcSinglePushService = injector.createChildInjector(
                 new AbstractModule() {
                     @Override
                     protected void configure() {
@@ -27,8 +31,7 @@ public class DormDaoJdbc implements DormDao {
 
     @Override
     public DormMetadata getMetadataByQualifier(final String qualifier) {
-        JDBCRetrieveByQualifierService jdbcRetrieveByQualifierService = Guice.createInjector(
-                new DormCoreModule(),
+        JDBCRetrieveByQualifierService jdbcRetrieveByQualifierService = injector.createChildInjector(
                 new AbstractModule() {
                     @Override
                     protected void configure() {
@@ -40,8 +43,7 @@ public class DormDaoJdbc implements DormDao {
 
     @Override
     public List<DormMetadata> getMetadataByExtension(final String extensionName, final Map<String, String> extensionClauses, Usage usage) {
-        JDBCRetrieveByExtensionClauseService jdbcRetrieveByExtensionClauseService = Guice.createInjector(
-                new DormCoreModule(),
+        JDBCRetrieveByExtensionClauseService jdbcRetrieveByExtensionClauseService = injector.createChildInjector(
                 new AbstractModule() {
                     @Override
                     protected void configure() {
