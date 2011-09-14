7e423c5179445355e37fcff0faea671286e02cb0
Lukasz Piliszczuk <lukasz.piliszczuk@zenika.com>
2011-09-14 17:06:54 +0200

    Clean maven web service.

--- plugins/maven/src/main/java/com/zenika/dorm/maven/ws/resource/MavenResource.java
+++ plugins/maven/src/main/java/com/zenika/dorm/maven/ws/resource/MavenResource.java
@@ -9,10 +9,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.*;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
 import java.io.File;
 
 /**
@@ -26,9 +24,6 @@ public class MavenResource extends AbstractResource {
 
     @Inject
     private Processor processor;
-
-    @Context
-    private UriInfo uriInfo;
 
     public MavenResource() {
         if (LOG.isInfoEnabled()) {
@@ -48,86 +43,6 @@ public class MavenResource extends AbstractResource {
 
         return Response.status(Response.Status.NOT_FOUND).build();
     }
-
-//        DormWebServiceRequest request = new DormRequestBuilder(version)
-//                .filename(filename)
-//                .property(MavenMetadataExtension.METADATA_GROUPID, MavenResourceHelper.formatGroupId(groupId))
-//                .property(MavenMetadataExtension.METADATA_ARTIFACTID, artifactId)
-//                .property(MavenMetadataExtension.METADATA_VERSION, version)
-//                .build();
-//
-//        if (LOG.isInfoEnabled()) {
-//            LOG.info("GET request to the maven web service : " + request);
-//        }
-//
-//        Dependency dependency = processor.get(request);
-//
-//        if (null == dependency.getResource() || null == dependency.getResource().getFile() || !dependency.getResource().getFile().exists()) {
-//            LOG.info("Return http response 404");
-//            return Response.status(Response.Status.NOT_FOUND).build();
-//        }
-//
-//        if (LOG.isInfoEnabled()) {
-//            LOG.info("Return response http 200 with the dependency : " + dependency);
-//        }
-//
-//        return Response.status(Response.Status.OK).entity(dependency.getResource().getFile()).build();
-//    }
-
-//    @PUT
-//    @Consumes(MediaType.APPLICATION_OCTET_STREAM)
-//    @Path("{groupId:.*}/{artifactId}/{version}/{fileName}")
-//    public Response put(File file,
-//                        @PathParam("groupId") String groupId,
-//                        @PathParam("artifactId") String artifactId,
-//                        @PathParam("version") String version,
-//                        @PathParam("fileName") String fileName) {
-//
-//        LOG.info("Call to maven web service : PUT");
-//
-//        DormWebServiceRequest request = new DormRequestBuilder(MavenMetadataExtension.EXTENSION_NAME)
-//                .file(file)
-//                .filename(fileName)
-//                .property(MavenMetadataExtension.METADATA_GROUPID, MavenResourceHelper.formatGroupId(groupId))
-//                .property(MavenMetadataExtension.METADATA_ARTIFACTID, artifactId)
-//                .property(MavenMetadataExtension.METADATA_VERSION, version)
-//                .build();
-//
-//        LOG.info("PUT request to the maven web service : " + request);
-//
-//        if (processor.push(request)) {
-//            return Response.status(Response.Status.OK).build();
-//        }
-//
-//        return Response.status(Response.Status.NOT_FOUND).build();
-//    }
-
-
-//    @PUT
-//    @Consumes(MediaType.APPLICATION_OCTET_STREAM)
-//    @Path("{path:.*}/{filename}")
-//    public Response put(
-//            @PathParam("path") String path,
-//            @PathParam("filename") String filename,
-//            @FormParam("file") File file) {
-////                        @PathParam("file") InputStream inputStream) {
-//
-//        if (LOG.isInfoEnabled()) {
-//            LOG.info("Maven webservice PUT with path : " + path + " and filename : " + filename);
-//        }
-//
-////        File file = DormFileUploadHelper.getUploadedFile(inputStream);
-//
-//        DormWebServiceRequest request = new DormWebServiceRequestBuilder(MavenMetadataExtension.EXTENSION_NAME)
-//                .file(file)
-//                .filename(filename)
-//                .property("path", path)
-//                .build();
-//
-//        processor.push(request);
-//
-//        return Response.status(Response.Status.OK).build();
-//    }
 
     @PUT
     @Path("{path:.*}/{filename}")
