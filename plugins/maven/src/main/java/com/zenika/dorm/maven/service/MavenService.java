3299dbaed17d0800636b23c555bbccc519d810cd
Lukasz Piliszczuk <lukasz.piliszczuk@zenika.com>
2011-09-30 19:07:36 +0200

    Parsing of the pom.xml during the push.

--- plugins/maven/src/main/java/com/zenika/dorm/maven/service/MavenService.java
+++ plugins/maven/src/main/java/com/zenika/dorm/maven/service/MavenService.java
@@ -1,22 +1,21 @@
 package com.zenika.dorm.maven.service;
 
 import com.google.inject.Inject;
+import com.zenika.dorm.core.model.Dependency;
 import com.zenika.dorm.core.model.DependencyNode;
-import com.zenika.dorm.core.model.DormMetadata;
 import com.zenika.dorm.core.model.DormResource;
 import com.zenika.dorm.core.model.impl.DefaultDependency;
 import com.zenika.dorm.core.model.impl.DefaultDependencyNode;
 import com.zenika.dorm.core.model.impl.DefaultDormResource;
 import com.zenika.dorm.core.service.DormService;
 import com.zenika.dorm.core.service.config.DormServiceStoreResourceConfig;
+import com.zenika.dorm.maven.exception.MavenException;
 import com.zenika.dorm.maven.model.MavenMetadata;
-import com.zenika.dorm.maven.model.builder.MavenMetadataBuilder;
+import com.zenika.dorm.maven.pom.MavenPomReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author Lukasz Piliszczuk <lukasz.piliszczuk AT zenika.com>
@@ -66,42 +65,24 @@ public class MavenService {
             LOG.debug("Store maven pom from metadata : " + metadata);
         }
 
+        MavenPomReader reader = new MavenPomReader(file);
+        MavenMetadata pomMetadata = reader.getArtifact();
+
+        if (!metadata.equals(pomMetadata)) {
+            throw new MavenException("Artifact to store and associated pom are different");
+        }
+
+        DependencyNode root = DefaultDependencyNode.create(DefaultDependency.create(metadata));
+
+        for (Dependency dependency : reader.getDependencies()) {
+            root.addChild(DefaultDependencyNode.create(dependency));
+        }
+
         DormResource resource = DefaultDormResource.create(file);
         DormServiceStoreResourceConfig config = new DormServiceStoreResourceConfig()
                 .override(true);
 
         dormService.storeResource(resource, metadata, config);
-
-        // read pom
-        MavenMetadata metadata1 = new MavenMetadataBuilder()
-                .groupId("group1")
-                .artifactId("artifact1")
-                .version("1.0")
-                .build();
-
-        MavenMetadata metadata2 = new MavenMetadataBuilder()
-                .groupId("group2")
-                .artifactId("artifact2")
-                .version("2.0")
-                .build();
-
-        MavenMetadata metadata3 = new MavenMetadataBuilder()
-                .groupId("group3")
-                .artifactId("artifact3")
-                .version("3.0")
-                .build();
-
-        List<DormMetadata> dependenciesMetadatas = new ArrayList<DormMetadata>();
-        dependenciesMetadatas.add(metadata1);
-        dependenciesMetadatas.add(metadata2);
-        dependenciesMetadatas.add(metadata3);
-
-        DependencyNode root = DefaultDependencyNode.create(DefaultDependency.create(metadata));
-
-        for (DormMetadata dependencyMetadata : dependenciesMetadatas) {
-            root.addChild(DefaultDependencyNode.create(DefaultDependency.create(dependencyMetadata)));
-        }
-
         dormService.addDependenciesToNode(root);
     }
 
