3ad5699b8145c7efc134139245d2d4caf2d0b9b3
Antoine Rouaze <antoine.rouaze@zenika.com>
2011-12-01 22:02:51 +0100

    Add DORM tests functional

---
+++ demo/maven_integration_test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+while getopts "p:h:" optname
+  do
+    case "$optname" in
+      "p")
+	PORT=$OPTARG
+	;;
+      "h")
+	HOST=$OPTARG
+	;;
+      "?")
+	echo "Unknow option $OPTARG"
+	;;
+      ":")
+	echo "No argument value for option $OPTARG"
+	;;
+      *)
+	echo "Unknown error while processing options"
+	;;
+    esac
+  done
+echo $HOST
+echo $PORT
+java -jar maven_importer.jar -h localhost -p 8080 -P maven -l lib_to_deploy/ 
+mvn archetype:create -DgroupId=com.zenika.test -DartifactId=test -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeGroupId=org.apache.maven.archetypes
+sed -f sed_script test/pom.xml > test/pom2.xml
+mv test/pom2.xml test/pom.xml
+cd test
+mvn install
+mvn deploy
